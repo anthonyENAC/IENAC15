e30e9b6cfc440c90a2b03d3fc48719a61022c6ad
anthonyENAC <anthony.aman@hotmail.fr>
2016-05-09 23:02:36 +0200

    Last version 09/05 23h02

--- entete.php
+++ entete.php
@@ -4,20 +4,20 @@
     <meta charset="utf-8" />
     <title>GRIT</title>
     <link rel="stylesheet" href="css/style.css" type="text/css" />
-    <link rel='stylesheet' type='text/css' href="/IENAC15/aman_begaud_gaulmin_thirion/css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="/IENAC15/aman_begaud_gaulmin_thirion/css/bootstrap.min.css"/>
+    <link rel='stylesheet' type='text/css' href="css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
     <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/themes/base/jquery-ui.css" type="text/css" media="all" />
     <link rel="stylesheet" href="http://static.jquery.com/ui/css/demo-docs-theme/ui.theme.css" type="text/css" media="all" />
-    <link rel="stylesheet" href="/IENAC15/aman_begaud_gaulmin_thirion/css/style.css" type="text/css" media="all" />
+    <!--<link rel="stylesheet" href="/IENAC15/aman_begaud_gaulmin_thirion/css/style.css" type="text/css" media="all" />-->
     <link rel="stylesheet" type="text/css" href="/IENAC15/aman_begaud_gaulmin_thirion/css/bootstrap-table.min.css"/>
 
         <!-- <script type="text/javascript" src="/IENAC15/aman_begaud_gaulmin_thirion/js/jquery.js"></script>-->
-    <script type="text/javascript" src="/IENAC15/aman_begaud_gaulmin_thirion/js/jquery-2.2.1.min.js"></script>
-    <script type="text/javascript" src="/IENAC15/aman_begaud_gaulmin_thirion/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/IENAC15/aman_begaud_gaulmin_thirion/js/event.js"></script>
-    <script type="text/javascript" src="/IENAC15/aman_begaud_gaulmin_thirion/js/jquery.min.js"></script>
-    <script type="text/javascript" src="/IENAC15/aman_begaud_gaulmin_thirion/js/jquery-ui.min.js" ></script>
-    <script type="text/javascript" src="/IENAC15/aman_begaud_gaulmin_thirion/js/bootstrap-table.min.js" ></script>
+    <script type="text/javascript" src="js/jquery-2.2.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/event.js"></script>
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.min.js" ></script>
+    <script type="text/javascript" src="js/bootstrap-table.min.js" ></script>
 </head>
 
 <body>
