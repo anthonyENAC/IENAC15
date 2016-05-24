2b820d64f2383333b0f53402492f15c60404b611
anthonyENAC <anthony.aman@hotmail.fr>
2016-05-24 23:49:20 +0200

    Dernière version 24/05  23h49

--- admin.php
+++ admin.php
@@ -2,7 +2,7 @@
 <?php
 try
 {
-     $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
+     $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', '');
 }
 
 catch(Exception $e)
--- register.php
+++ register.php
@@ -1,5 +1,14 @@
 <?php include("entete.php"); ?>
 
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+    <body>
+    <div align="center">
+        <h2>Inscription</h2>
+        <br />
+
 <?php
 
 $bdd=new PDO('mysql:host=localhost;dbname=GRIT', 'root', '');
@@ -11,6 +20,7 @@ if(isset($_POST['forminscription']))
     $email=htmlspecialchars($_POST['email']);
     $mdp=sha1($_POST['mdp']);
     $mdp2=sha1($_POST['mdp2']);
+    $date_naiss=date($_POST['date_naiss']);
 
     if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['email']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2']))
     {
@@ -27,106 +37,151 @@ if(isset($_POST['forminscription']))
                     $mailexist=$reqmail->rowCount();
                     if($mailexist==0)
                     {
-                        if ($mdp == $mdp2) {
-                            $insertmbr = $bdd->prepare("INSERT INTO Utilisateur(nom, prenom, email, mdp) VALUES(?, ?, ?, ?)");
-                            $insertmbr->execute(array($nom, $prenom, $email, $mdp));
-                            $erreur = "Votre compte a bien été créé !";
-                            header('Location: index.php');
-                        } else {
-                            $erreur = "Vos mots de passe ne correscpondent pas";
+                        if ($mdp == $mdp2)
+                        {
+
+                            if ($mdp >= 6)
+                            {
+                                    $dn = mysql_num_rows(mysql_query('SELECT id_user FROM Utilisateur WHERE email="' . $email . '"'));
+                                    if ($dn == 0)
+                                    {
+                                    $dn2 = mysql_num_rows(mysql_query('select id_user from Utilisateur'));
+                                    $id_user = $dn2 + 1;
+                                    $insertmbr = $bdd->prepare("INSERT INTO Utilisateur(id_user, nom, prenom, email, mdp, date_naiss) VALUES(?, ?, ?, ?, ?, ?)");
+                                    $insertmbr->execute(array($id_user, $nom, $prenom, $email, $mdp, $date_naiss));
+                                    $form = false;
+                                    }
+                            ?>
+                            <div class="message" align="center">Vous avez bien été inscrit. Vous pouvez désormais vous
+                                connecter avec vos identifiants.<br/>
+                                <a href="connexion.php">Se connecter</a></div>
+                            <?php
+                            }
+                            else
+                            {
+                                $form=true;
+                                $erreur="Votre mot de passe n'est pas assez long.";
+
+                            }
+                        }
+                        else
+                        {
+                            $form=true;
+                            $erreur = "Vos mots de passe ne correscpondent pas.";
                         }
                     }
                     else
                     {
+                        $form=true;
                         $erreur = "Adresse mail déjà utilisée !";
                     }
                 }
                 else
                 {
+                    $form=true;
                     $erreur="Votre adresse mail n'est pas valide !";
                 }
             }
 
             else
             {
+                $form=true;
                 $erreur="Votre prenom ne doit pas dépasser 20 caractères !";
             }
 
         }
         else
         {
+            $form=true;
             $erreur="Votre nom ne doit pas dépasser 20 caractères !";
         }
 
     }
     else
     {
+        $form=true;
         $erreur="Tous les champs doivent être complétés";
     }
 }
-?>
-
-<html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <div align="center">
-            <h2>Inscription</h2>
-            <br /><br />
-            <?php
-            if(isset($erreur))
-            {
-                echo "<font color='red''>".$erreur."</font>";
-            }
+else
+{
+    $form=true;
+}
+if($form)
+{
+    if (isset($erreur))
+    {
+        echo "<font color='red'>" . $erreur . "</font>";
+    }
+                ?>
+    <h5>Veuillez remplir le formulaire ci-dessous.</h5>
+    <br /><br />
+                <form method="POST" action="">
+                    <table>
+                        <tr>
+                            <td align="right">
+                                <label for="nom">Nom : </label>
+                            </td>
+                            <td>
+                                <input type="text" placeholder="Nom" id="nom" name="nom" value="<?php if (isset($nom)) {
+                                    echo $nom;
+                                } ?>"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="prenom">Prenom : </label>
+                            </td>
+                            <td>
+                                <input type="text" placeholder="Prenom" id="prenom" name="prenom"
+                                       value="<?php if (isset($prenom)) {
+                                           echo $prenom;
+                                       } ?>"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="email">Email : </label>
+                            </td>
+                            <td>
+                                <input type="email" placeholder="Email" id="email" name="email"
+                                       value="<?php if (isset($email)) {
+                                           echo $email;
+                                       } ?>"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="mdp">Mot de passe (min 6 caractères) : </label>
+                            </td>
+                            <td>
+                                <input type="password" placeholder="Password" id="mdp" name="mdp"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="mdp2">Confirmez votre mot de passe : </label>
+                            </td>
+                            <td>
+                                <input type="password" placeholder="Confirmez mdp" id="mdp2" name="mdp2"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="right">
+                                <label for="date_naiss">Date de naissance format AAAA-MM-JJ :</label>
+                            </td>
+                            <td>
+                                <input type="date" id="date_naiss" name="date_naiss" placeholder="Date de naissance"/><br/>
+                            </td>
+                        </tr>
+                    </table>
+                    <br/>
+                    <input type="submit" name="forminscription" value="S'inscrire"/>
+                    <br/><br/>
+                </form>
+                <?php
+}
             ?>
-            <form method="POST" action="">
-                <table>
-                    <tr>
-                        <td align="right">
-                            <label for="nom">Nom : </label>
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Nom" id="nom" name="nom" value="<?php if(isset($nom)) {echo $nom;} ?>" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <label for="fprenom">Prenom : </label>
-                        </td>
-                        <td>
-                            <input type="text" placeholder="Prenom" id="prenom" name="prenom" value="<?php if(isset($prenom)) {echo $prenom;} ?>" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <label>Email : </label>
-                        </td>
-                        <td>
-                            <input type="email" placeholder="Email" id="email" name="email" value="<?php if(isset($email)) {echo $email;} ?>" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <label for="mdp">Mot de passe : </label>
-                        </td>
-                        <td>
-                            <input type="password" placeholder="Password" id="mdp" name="mdp" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="right">
-                            <label for="mdp2">Confirmez votre mot de passe : </label>
-                        </td>
-                        <td>
-                            <input type="password" placeholder="Confirmez mdp" id="mdp2" name="mdp2" />
-                        </td>
-                    </tr>
-                </table>
-                <br />
-                <input type="submit" name="forminscription" value="S'inscrire" />
-                <br /><br />
-            </form>
         </div>
     </body>
     <?php include("footer.php"); ?>
