a6967b47e38a479174cacf22a617fa1dd32f2e1a
mgaulmin <matthieu.gaulmin@gmail.com>
2016-05-26 16:50:49 +0200

    modif page admin

--- admin.php
+++ admin.php
@@ -2,70 +2,38 @@
 session_start();
 ?>
 
-<div id="admin" align="center" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+<div id="admin" align="center">
      <h2>Bienvenue sur la page d'administration</h2>
      <h5>Vous pouvez ici gérer la base de données du site de la GRIT</h5>
     
 <form method="post" action="submit.php">
     <div id="ajout_mode_type" class="center">
-            </br>Ajouter un type de mode de transport :</br>
+            </br>Passer un utilisateur en administrateur :</br>
             <label>Nom</label>
-            <input type="text" name="new_type" />
+            <select name="ajout_admin">
+                <?php
+                try
+                {
+                    $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
+                }
+                catch (Exception $e)
+                {
+                    die('Erreur : ' . $e->getMessage());
+                }
+
+                $reponse = $bdd->query('SELECT * FROM Utilisateur WHERE Utilisateur.id_user NOT IN (SELECT * FROM Administrateur)');
+
+                while ($donnees3 = $reponse->fetch())
+                {
+                    ?>
+                    <option value="<?php echo $donnees3['id_user'] ?>"><?php echo "{$donnees3['prenom']} {$donnees3['nom']}" ?></option>
+                    <?php
+                }
+                ?>
+            </select>
             <input type="submit" value="Ajouter" />
         </div>
-    </form>    
-    
-<form method="post" action="submit.php">
-    <div id="ajout_mode_transp" class="center">
-        </br>Ajouter un mode de transport :<br/>
-        <label>Etat du mode de transport</label>
-        <select name="etat_mode">
-            <?php
-            try
-            {
-                $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
-            }
-            catch (Exception $e)
-            {
-                die('Erreur : ' . $e->getMessage());
-            }
-
-            $reponse = $bdd->query('SELECT * FROM Etat');
-
-            while ($donnees = $reponse->fetch())
-            {
-                ?>
-                <option value="<?php echo $donnees['id_etat'] ?>"><?php echo $donnees['etat'] ?></option>
-                <?php
-            }
-            ?>
-        </select></br>
-
-        <label>Type de mode de transport</label>
-        <select name="type_mode">
-            <?php
-            try
-            {
-                $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
-            }
-            catch (Exception $e)
-            {
-                die('Erreur : ' . $e->getMessage());
-            }
-
-            $reponse = $bdd->query('SELECT * FROM Mode_Type');
-
-            while ($donnees2 = $reponse->fetch())
-            {
-            ?>
-            <option value="<?php echo $donnees2['id_type'] ?>"><?php echo $donnees2['descr_type'] ?></option>
-            <?php
-            }
-            ?>
-        </select></br>
-        <input type="submit" value="Ajouter" />
-    </div>
-</form>
+    </form>
 
 <form method="post" action="submit.php">
     <div id="ajout_mode_type" class="center">
@@ -81,10 +49,148 @@ session_start();
         </br>Ajouter une station :</br>
         <label>Nom</label>
         <input type="text" name="nom_station" /></br>
-        <label>Latitude</label>
+        <label>Latitude (°)</label>
         <input type="text" name="lat" /></br>
-        <label>Longitude</label>
+        <label>Longitude (°)</label>
         <input type="text" name="lng" /></br>
+        <input type="submit" value="Ajouter" />
+    </div>
+</form>
+
+<form method="post" action="submit.php">
+    <div id="ajout_transp_troncon" class="center">
+        </br>Ajouter un nouveau transport pour un tronçon :</br>
+        <label>Durée (s)</label>
+        <input type="text" name="new_duree" /></br>
+        <label>Heure de passage</label>
+        <input type="time" name="new_heure" /></br>
+        <label>Prix (€)</label>
+        <input type="text" name="new_prix" /></br>
+        <label>Tronçon</label>
+        <select name="troncon">
+            <?php
+            try
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
+            }
+            catch (Exception $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
+
+            $reponse = $bdd->query('SELECT * FROM Troncon');
+
+            while ($donnees4 = $reponse->fetch())
+            {
+                ?>
+                <option value="<?php echo $donnees4['id_troncon'] ?>"><?php echo $donnees4['nom_troncon'] ?></option>
+                <?php
+            }
+            ?>
+        </select>
+        <div id="ajout_mode_transp" class="center">
+            <label>Etat du mode de transport</label>
+            <select name="etat_mode">
+                <?php
+                try
+                {
+                    $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
+                }
+                catch (Exception $e)
+                {
+                    die('Erreur : ' . $e->getMessage());
+                }
+
+                $reponse = $bdd->query('SELECT * FROM Etat');
+
+                while ($donnees = $reponse->fetch())
+                {
+                    ?>
+                    <option value="<?php echo $donnees['id_etat'] ?>"><?php echo $donnees['etat'] ?></option>
+                    <?php
+                }
+                ?>
+            </select></br>
+
+            <label>Type de mode de transport</label>
+            <select name="type_mode">
+                <?php
+                try
+                {
+                    $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
+                }
+                catch (Exception $e)
+                {
+                    die('Erreur : ' . $e->getMessage());
+                }
+
+                $reponse = $bdd->query('SELECT * FROM Mode_Type');
+
+                while ($donnees2 = $reponse->fetch())
+                {
+                    ?>
+                    <option value="<?php echo $donnees2['id_type'] ?>"><?php echo $donnees2['descr_type'] ?></option>
+                    <?php
+                }
+                ?>
+            </select></br>
+        </div>
+        <input type="submit" value="Ajouter" />
+    </div>
+</form>
+
+<form method="post" action="submit.php">
+    <div id="ajout_mode_type" class="center">
+        </br>Ajouter un tronçon :</br>
+        <label>Nom</label>
+        <input type="text" name="nom_troncon" /></br>
+        <label>Station de départ</label>
+        <select name="station_dep">
+            <?php
+            try
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
+            }
+            catch (Exception $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
+
+            $reponse = $bdd->query('SELECT * FROM Station');
+
+            while ($donnees5 = $reponse->fetch())
+            {
+                ?>
+                <option value="<?php echo $donnees5['id_station'] ?>"><?php echo $donnees5['nom_station'] ?></option>
+                <?php
+            }
+            ?>
+        </select></br>
+        <label>Station d'arrivée</label>
+        <select name="station_arr">
+            <?php
+            try
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
+            }
+            catch (Exception $e)
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
+
+            $stat1 = $_POST['station_dep'];
+
+
+            $reponse = $bdd->query('SELECT * FROM Station');
+
+            while ($donnees6 = $reponse->fetch())
+            {
+                ?>
+                <option value="<?php echo $donnees6['id_station'] ?>"><?php echo $donnees6['nom_station'] ?></option>
+                <?php
+            }
+            ?>
+        </select></br>
         <input type="submit" value="Ajouter" />
     </div>
 </form>
--- submit.php
+++ submit.php
@@ -8,35 +8,16 @@ if ($mysqli->connect_errno) {
     }
 
 
-$option1 = isset($_POST['etat_mode'],$_POST['type_mode']) ? true : false;
-$option2 = isset($_POST['new_type']) ? true : false;
-$option3 = isset($_POST['nom_station'], $_POST['lat'], $_POST['lng']) ? true : false;
 
-if($option1) {
-    $etat = htmlentities($_POST['etat_mode'], ENT_QUOTES, "UTF-8");
-    $type = htmlentities($_POST['type_mode'], ENT_QUOTES, "UTF-8");
-
-    if ($mysqli->query("INSERT INTO Mode_Transport VALUES ('', $etat, $type)") === TRUE)
-    {
-       ?>
-        <div align="center">
-            <img src="images/ok.svg" height="128px" width="128px">
-            <h4>Les nouvelles données ont bien été ajoutées à la base de données.</h4>
-        </div>
-    <?php
-    }
-    else
-        ?>
-        <div align="center">
-            <img src="images/error.png" height="128px" width="128px">
-            <h4>Problème détécté dans l'ajout des données.</h4>
-        </div>
-    <?php ;
-    $mysqli->close();
-    }
+$option1 = isset($_POST['new_type']) ? true : false;
+$option2 = isset($_POST['nom_station'], $_POST['lat'], $_POST['lng']) ? true : false;
+$option3 = isset($_POST['ajout_admin']) ? true : false;
+$option4 = isset($_POST['new_duree'], $_POST['new_heure'], $_POST['new_prix'], $_POST['troncon'], $_POST['etat_mode'],
+    $_POST['type_mode']) ? true : false;
+$option5 = isset($_POST['station_dep'], $_POST['station_arr']) ? true : false;
 
 
-if ($option2) {
+if ($option1) {
     $new_type = htmlentities($_POST['new_type'], ENT_QUOTES, "UTF-8");
 
     if ($mysqli->query("INSERT INTO Mode_Type VALUES ('', '$new_type')") === TRUE)
@@ -48,17 +29,18 @@ if ($option2) {
         </div>
     <?php
     }
-    else
+    else {
         ?>
         <div align="center">
             <img src="images/error.png" height="128px" width="128px">
             <h4>Problème détécté dans l'ajout des données.</h4>
         </div>
         <?php ;
+    }
     $mysqli->close();
     }
 
-if ($option3) {
+if ($option2) {
     $nom_station = htmlentities($_POST['nom_station'], ENT_QUOTES, "UTF-8");
     $lat = htmlentities($_POST['lat'], ENT_QUOTES, "UTF-8");
     $lng = htmlentities($_POST['lng'], ENT_QUOTES, "UTF-8");
@@ -72,16 +54,106 @@ if ($option3) {
         </div>
         <?php
     }
-    else
+    else {
         ?>
         <div align="center">
             <img src="images/error.png" height="128px" width="128px">
             <h4>Problème détécté dans l'ajout des données.</h4>
         </div>
-    <?php ;
+        <?php ;
+    }
     $mysqli->close();
 }
 
+if ($option3) {
+    $ajout_admin = htmlentities($_POST['ajout_admin'], ENT_QUOTES, "UTF-8");
+
+    if ($mysqli->query("INSERT INTO Administrateur VALUES ($ajout_admin)") === TRUE)
+    {
+        ?>
+        <div align="center">
+            <img src="images/ok.svg" height="128px" width="128px">
+            <h4>Les nouvelles données ont bien été ajoutées à la base de données.</h4>
+        </div>
+        <?php
+    }
+    else {
+        ?>
+        <div align="center">
+            <img src="images/error.png" height="128px" width="128px">
+            <h4>Problème détécté dans l'ajout des données.</h4>
+        </div>
+        <?php ;
+    }
+    $mysqli->close();
+}
+
+if ($option4) {
+    $etat = htmlentities($_POST['etat_mode'], ENT_QUOTES, "UTF-8");
+    $type = htmlentities($_POST['type_mode'], ENT_QUOTES, "UTF-8");
+    $new_duree = htmlentities($_POST['new_duree'], ENT_QUOTES, "UTF-8");
+    $new_heure1 = htmlentities($_POST['new_heure'], ENT_QUOTES, "UTF-8");
+    $new_heure2 = explode(":", $new_heure1);
+    $new_heure = intval($new_heure2[0])*3600 + intval($new_heure2[1])*60;
+    $new_prix = htmlentities($_POST['new_prix'], ENT_QUOTES, "UTF-8");
+    $troncon = htmlentities($_POST['troncon'], ENT_QUOTES, "UTF-8");
+    
+    if ($mysqli->query("INSERT INTO Mode_Transport VALUES ('', $etat, $type)") === TRUE)
+    {
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=GRIT;charset=utf8', 'root', 'mysql');
+        }
+        catch (Exception $e)
+        {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        $reponse3 = $bdd->query('SELECT MAX(id_transport) AS max_id FROM Mode_Transport');
+        $id_transp = $reponse3->fetch();
+        if ($mysqli->query("INSERT INTO Transport_Troncon VALUES ('', $new_duree, $new_heure, $new_prix, $id_transp[0], $troncon)") === TRUE)
+        ?>
+        <div align="center">
+            <img src="images/ok.svg" height="128px" width="128px">
+            <h4>Les nouvelles données ont bien été ajoutées à la base de données.</h4>
+        </div>
+        <?php
+    }
+    else {
+        ?>
+        <div align="center">
+            <img src="images/error.png" height="128px" width="128px">
+            <h4>Problème détécté dans l'ajout des données.</h4>
+        </div>
+        <?php ;
+    }
+    $mysqli->close();
+}
+
+if ($option5) {
+    $station_dep = htmlentities($_POST['station_dep'], ENT_QUOTES, "UTF-8");
+    $station_arr = htmlentities($_POST['station_arr'], ENT_QUOTES, "UTF-8");
+    $nom_troncon = htmlentities($_POST['nom_troncon'], ENT_QUOTES, "UTF-8");
+
+    if ($mysqli->query("INSERT INTO Troncon VALUES ('', '$nom_troncon', $station_dep, $station_arr)") === TRUE)
+    {
+        ?>
+        <div align="center">
+            <img src="images/ok.svg" height="128px" width="128px">
+            <h4>Les nouvelles données ont bien été ajoutées à la base de données.</h4>
+        </div>
+        <?php
+    }
+    else {
+        ?>
+        <div align="center">
+            <img src="images/error.png" height="128px" width="128px">
+            <h4>Problème détécté dans l'ajout des données.</h4>
+        </div>
+        <?php ;
+    }
+    $mysqli->close();
+}
 ?>
 
 <div align="center">
